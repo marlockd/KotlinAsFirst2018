bca60a3395590939cc78a8452609362c99aeba7e
marlockd <denislogvinov42@gmail.com>
2018-12-16 20:19:57 +0300

    7 lesson v.8

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -282,7 +282,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
 
     }
     for (i in 0 until str.size) {
-        if ((str[i] == "") && (str[i - 1] != "")) {
+        if ((str[i] == "") && (str[i - 1] != "") && (i != 0)) {
             writer.write("\n\t\t</p>\n\t\t<p>\n\t\t\t")
 
         } else {
